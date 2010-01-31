419d89e00aa21968da5ddcd71d22053311b8a245
Robin <robin@basicinside.loves.fun>
2010-01-31 17:04:23 +0100

    update script fix, kartenansicht link fix, remove crap from repo

--- db/migrate/20100130214525_add_last_seen_to_node.rb
+++
@@ -1,9 +0,0 @@
-class AddLastSeenToNode < ActiveRecord::Migration
-  def self.up
-    add_column :nodes, :last_seen, :timestamp
-  end
-
-  def self.down
-    remove_column :nodes, :last_seen
-  end
-end
