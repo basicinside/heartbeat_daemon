280b1911d52b5e6c07f2be3d643be306149a764e
Robin <robin@basicinside.loves.fun>
2010-01-31 15:00:44 +0100

    map popup fix

--- app/models/node.rb
+++ app/models/node.rb
@@ -3,4 +3,16 @@ class Node < ActiveRecord::Base
   belongs_to :partei
   belongs_to :crew
   has_many :scores, :dependent => :destroy
+  
+  def score_count
+  	Score.sum('score', :conditions => ['node_id = ?', id])
+  end
+  
+  def popup_info
+  "<h4><a href='/nodes/#{id}'>#{name}</a></h4>
+  Firmware #{rev}<br />
+  <a href='/landesverbands/#{landesverband.id}'>Crew #{crew.name}</a><br />
+  <a href='/crews/#{crew.id}'>LV #{landesverband.name}</a><br />
+ 	#{score_count} Punkte"
+  end
 end
